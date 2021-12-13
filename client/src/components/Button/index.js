577d65e198c72e23f0cd900bbe5983f5f3f23129
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-13 14:54:56 +0200

    create forms with formik and Yup for validations for authentication

---
+++ client/src/components/Button/index.js
@@ -0,0 +1,16 @@
+import './Button.css';
+
+const Button = (props) => {
+	return (
+		<button
+			type={props.type}
+			className={props.className}
+			onClick={props.onClick}
+			disabled={props.disabled || false}
+		>
+			{props.children}
+		</button>
+	);
+};
+
+export default Button;
