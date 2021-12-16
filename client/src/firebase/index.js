f57e96b4c7fc19a9a479f2597332fc2bc97f17f5
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-12-16 13:21:29 +0200

    initialize firebase to my project , I will use only the storage to store images

---
+++ client/src/firebase/index.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from 'firebase/storage';
+
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAgunaHCgK-es_b0HmVTjGXYdeYHyxODeU",
+	authDomain: "adopt-a-pet-ae0fb.firebaseapp.com",
+	projectId: "adopt-a-pet-ae0fb",
+	storageBucket: "adopt-a-pet-ae0fb.appspot.com",
+	messagingSenderId: "1061977746819",
+	appId: "1:1061977746819:web:99ace837bd4b1af26d5ab6"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
